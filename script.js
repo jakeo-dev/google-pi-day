d80d93b80b0e40e3ca2d26a73dc469fa927dc1e0
Jake <56329025+jakeo-dev@users.noreply.github.com>
2025-08-22 08:20:28 -0700

    update to include pixel 9

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = getFile;
 
 function getFile() {
     var txtFile = new XMLHttpRequest();
-    txtFile.open('GET', '/pi.txt', true);
+    txtFile.open("GET", "/pi.txt", true);
     txtFile.onreadystatechange = function () {
         if (txtFile.readyState === 4) {
             if (txtFile.status === 200) {
@@ -15,30 +15,32 @@ function getFile() {
 }
 
 function doThing() {
-    let img = document.createElement('img');
-    img.src = 'images/3.png';
+    let img = document.createElement("img");
+    img.src = "images/3.png";
     img.width = 75;
     img.height = 150;
-    img.id = '0';
-    document.getElementById('piDiv').appendChild(img);
+    img.id = "0";
+    document.getElementById("piDiv").appendChild(img);
 
-    img = document.createElement('img');
-    img.src = 'images/D.png';
+    img = document.createElement("img");
+    img.src = "images/D.png";
     img.width = 10;
     img.height = 10;
-    img.id = 'D';
-    document.getElementById('piDiv').appendChild(img);
+    img.id = "D";
+    document.getElementById("piDiv").appendChild(img);
 
     for (i = 2; i < 10002; i++) {
-        let img = document.createElement('img');
+        let img = document.createElement("img");
         img.id = i;
 
-        if (pi[i] == 9) {
-            img.src = 'images/6.png';
-            img.style.rotate = '180deg';
+        img.src = "images/" + pi[i] + ".png";
+
+        /* if (pi[i] == 9) {
+            img.src = "images/6.png";
+            img.style.rotate = "180deg";
         } else {
-            img.src = 'images/' + pi[i] + '.png';
-        }
+            img.src = "images/" + pi[i] + ".png";
+        } */
 
         if (pi[i] == 8) {
             img.width = 83;
@@ -48,6 +50,6 @@ function doThing() {
             img.height = 150;
         }
 
-        document.getElementById('piDiv').appendChild(img);
+        document.getElementById("piDiv").appendChild(img);
     }
 }
